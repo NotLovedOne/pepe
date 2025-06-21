d79f7c257fbe0fd631fe42adcdf038f0d98d6aa6
Yerassyl <124391362+NotLovedOne@users.noreply.github.com>
2025-06-21 15:11:14 +0600

    Remove per-service wrappers

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    kotlin("jvm") version "1.9.25" apply false
+    kotlin("plugin.spring") version "1.9.25" apply false
+    id("org.springframework.boot") version "3.5.3" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
+    kotlin("plugin.jpa") version "1.9.25" apply false
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+    }
+}
